7910474bfcc87d6f1e8ec4fd419dff5033827366
ysawiris <youssef.sawiris@students.dominican.edu>
2020-11-26 03:06:51 +0200

    Messages go to correct channel

--- sockets/chat.js
+++ sockets/chat.js
@@ -10,8 +10,10 @@ module.exports = (io, socket, onlineUsers, channels) => {
 	});
 
 	socket.on("new message", (data) => {
-		console.log(`🎤 ${data.sender}: ${data.message} 🎤`);
-		io.emit("new message", data);
+		//Save the new message to the channel.
+		channels[data.channel].push({ sender: data.sender, message: data.message });
+		//Emit only to sockets that are in that channel room.
+		io.to(data.channel).emit("new message", data);
 	});
 
 	socket.on("get online users", () => {
