32fa13bc473323bab448d1bf55c8462d09f4166a
ysawiris <youssef.sawiris@students.dominican.edu>
2020-11-26 03:09:30 +0200

    Implemented changing channels

--- sockets/chat.js
+++ sockets/chat.js
@@ -43,4 +43,11 @@ module.exports = (io, socket, onlineUsers, channels) => {
 			messages: channels[newChannel],
 		});
 	});
+	socket.on("user changed channel", (newChannel) => {
+		socket.join(newChannel);
+		socket.emit("user changed channel", {
+			channel: newChannel,
+			messages: channels[newChannel],
+		});
+	});
 };
