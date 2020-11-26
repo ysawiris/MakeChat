76f9d5ba2077f73c2c000a3539ce95546831d61f
ysawiris <youssef.sawiris@students.dominican.edu>
2020-11-26 02:13:41 +0200

    Users can log in

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-//App.js
+//app.js
 const express = require("express");
 const app = express();
-//Socket.io has to use the http server
 const server = require("http").Server(app);
 
-//Express View Engine for Handlebars
+//Socket.io
+const io = require("socket.io")(server);
+io.on("connection", (socket) => {
+	// This file will be read on new socket connections
+	require("./sockets/chat.js")(io, socket);
+	console.log("🔌 New user connected! 🔌");
+});
+
 const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
+//Establish your public folder
+app.use("/public", express.static("public"));
+
 app.get("/", (req, res) => {
-	res.render("index.handlebars");
+	res.render("layouts/main.handlebars");
 });
 
 server.listen("3000", () => {
